6ead07728c248b1f52711f7dd6e085804d9b862e
RaghavSobti37 <raghavsobti37@gmail.com>
2026-05-20 09:53:02 +0530

    feat: implement email geo-analytics tracking and campaign send idempotency

--- server/routes/analyticsRoutes.js
+++ server/routes/analyticsRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const analyticsController = require('../controllers/analyticsController');
+const mailAnalyticsController = require('../controllers/mailAnalyticsController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/cumulative', protect, analyticsController.getCumulativeMetrics);
 router.get('/location-leads', protect, analyticsController.getLocationLeads);
+router.get('/geo-campaign', protect, mailAnalyticsController.getGeoCampaignAnalytics);
 
 module.exports = router;
